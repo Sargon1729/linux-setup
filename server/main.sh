a04910f4106f380a89727f883c19adbc86016c40
Sargon1729 <artur.popielski1@gmail.com>
2025-07-25 00:36:59 +0100

    changed file names of packages.txt and server folder to basic

--- server/main.sh
+++
@@ -1,7 +0,0 @@
-apt install $(cat packages.txt) -y
-
-#DOPS for better docker ps
-wget "https://github.com/Mikescher/better-docker-ps/releases/latest/download/dops_linux-amd64-static" -O "/usr/local/bin/dops" && chmod +x "/usr/local/bin/dops"
-
-####comment for testing
-
