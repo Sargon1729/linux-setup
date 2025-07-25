32745366804d6e2682e3bc91365ae06d519e0ba7
Sargon1729 <artur.popielski1@gmail.com>
2025-07-25 18:45:36 +0100

    added line to create smb directory

--- basics/mount.sh
+++ basics/mount.sh
@@ -1,4 +1,5 @@
 touch /mnt/.smbcredentials
 chmod 600 /mnt/.smbcredentials
+mkdir /mnt/smb
 echo '//ajr1.sargon.local/smb /mnt/smb cifs credentials=/mnt/.smbcredentials,uid=1000,gid=1000,x-systemd.automount 0 0' >> /etc/fstab
 chmod -R 666 /mnt/smb
