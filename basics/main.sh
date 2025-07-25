f21f3277effc7fa4c472e6de363eedbda5163fc2
Sargon1729 <artur.popielski1@gmail.com>
2025-07-25 14:20:51 +0100

    added mount as a seperate scirpt

--- basics/main.sh
+++ basics/main.sh
@@ -7,7 +7,4 @@ wget "https://github.com/Mikescher/better-docker-ps/releases/latest/download/dop
 
 
 #Mount volumes################################################################################
-touch /mnt/.smbcredentials
-chmod 600 /mnt/.smbcredentials
-echo '//ajr1.sargon.local/smb /mnt/smb cifs credentials=/mnt/.smbcredentials,uid=1000,gid=1000,x-systemd.automount 0 0' >> /etc/fstab
-chmod -R 666 /mnt/smb
+#. main.sh
