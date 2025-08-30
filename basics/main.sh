1445331d08adb4706756402ac7fbdb8eae23e509
Sargon1729 <artur.popielski1@gmail.com>
2025-08-30 21:38:15 +0100

    proper docker install

--- basics/main.sh
+++ basics/main.sh
@@ -5,6 +5,8 @@ apt install $(cat basic_packages.txt) -y
 #DOPS for better docker ps
 wget "https://github.com/Mikescher/better-docker-ps/releases/latest/download/dops_linux-amd64-static" -O "/usr/local/bin/dops" && chmod +x "/usr/local/bin/dops"
 
+#Docker
+bash docker.sh
 
 #Mount volumes################################################################################
 #. mount.sh
